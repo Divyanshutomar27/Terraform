be4651b839d98daf93042c24950b601f31b17342
Divyanshutomar27 <36529072+Divyanshutomar27@users.noreply.github.com>
2021-07-01 19:11:45 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -11,14 +11,56 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = "ap-south-1"
 }
 
+module "vpc" {
+
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+
+  version = "2.78.0"
+
+  cidr =  var.vpc_cidr
+
+
+  azs = var.vpc_azs
+
+  private_subnets = var.vpc_private_sub
+
+  public_subnets  = var.vpc_public_sub
+
+  enable_nat_gateway = var.nat_gateway
+  enable_vpn_gateway = var.igw_gateway
+
+tags = var.vpc_tag
+
+}
+
+
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within vpc"
+  vpc_id      = "vpc-0b85bc0c6a8535375"
+
+  ingress_cidr_blocks = ["10.0.0.0/16"]
+}
+
+
+
 resource "aws_instance" "app_server" {
-  ami           = "ami-0dbd8c88f9060cf71"
+  ami           = "ami-0f6ddd5cd06387305"
   instance_type = "t2.micro"
 
+ subnet_id = module.vpc.private_subnets[1]
   tags = {
     Name = var.instance_name
   }
 }
+
+
+
+
