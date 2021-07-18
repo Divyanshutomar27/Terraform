9ff8988be4841d7f82b57ab504c3d477bed78e42
Divyanshutomar27 <36529072+Divyanshutomar27@users.noreply.github.com>
2021-07-18 18:21:28 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id"{
-        description = "Id of the EC2 Instnce"
-        value = aws_instance.app_server.id
-}
-
-output "instance_public_ip"{
-description = " Public Ip for Instance"
-value = aws_instance.app_server.public_ip
-}
